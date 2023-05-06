91fb82c56591229a4672b4f4d3e4578202c546e0
SergeyBirukov <birukovsergej121@gmail.com>
2023-05-07 04:35:25 +0500

    Added smoke when player take damage

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,10 +12,29 @@ namespace DefaultNamespace
         public int Health { get; private set; } = 100;
         public UnityEvent onDeath;
 
+        [SerializeField] private GameObject smoke1;
+        [SerializeField] private GameObject smoke2;
+        [SerializeField] private GameObject smoke3;
+
+
         public void TakeDamage(int damage)
         {
             Health -= damage;
             Debug.Log("damage taken");
+            if (Health <= 75)
+            {
+                smoke1.SetActive(true);
+            }
+
+            if (Health <= 50)
+            {
+                smoke2.SetActive(true);
+            }
+
+            if (Health <= 25)
+            {
+                smoke3.SetActive(true);
+            }
             if (Health <= 0)
             {
                 Die();
