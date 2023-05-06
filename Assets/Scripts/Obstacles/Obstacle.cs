772d8a194ea5743630227c5a9991e04b53c3038a
SergeyBirukov <birukovsergej121@gmail.com>
2023-05-06 18:36:40 +0500

    Added damage to player

--- Assets/Scripts/Obstacles/Obstacle.cs
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using DefaultNamespace;
+using UnityEngine;
 
 namespace Obstacles
 {
@@ -6,11 +8,23 @@ namespace Obstacles
     {
         [SerializeField] private Rigidbody2D _rigidbody2D;
         [SerializeField] private float speed;
+        [SerializeField] private int collisionDamage;
 
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
             _rigidbody2D.velocity = new Vector2(0, -speed);
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            Debug.Log("collision enter");
+            var player = col.gameObject.GetComponent<Player>();
+            if (player)
+            {
+                player.TakeDamage(collisionDamage);
+            }
+            Destroy(gameObject);
         }
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Security.Cryptography;
+using Interfaces;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DefaultNamespace
+{
+    public class Player : MonoBehaviour, IDamageable
+    {
+        [field:SerializeField]
+        public int Health { get; private set; } = 100;
+        public UnityEvent onDeath;
+        public static Player Instance;
+
+        private void Awake()
+        {
+            if (Instance is null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            Debug.Log("damage taken");
+            if (Health <= 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            onDeath.Invoke();
+        }
+    }
+}
