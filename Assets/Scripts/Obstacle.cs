30b41e2a33b9bc6ead70698ac427df97700b0f45
SergeyBirukov <birukovsergej121@gmail.com>
2023-05-06 17:41:12 +0500

    Added obstacleSpawner

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public abstract class Obstacle: MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D _rigidbody2D;
+        [SerializeField] private float speed;
+
+        private void Start()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _rigidbody2D.velocity = new Vector2(0, -speed);
+        }
+    }
+}
---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ObstacleSpawner: MonoBehaviour
+    {
+        [SerializeField] private SpawnArea spawnArea;
+        [SerializeField] private Obstacle dummyObstaclePrefab;
+        [SerializeField] private float spawnDelayInSeconds;
+
+        private void Spawn(Obstacle obstacle)
+        {
+            Instantiate(obstacle, spawnArea.GetRandomPosition(), Quaternion.identity);
+        }
+
+        private void Start()
+        {
+            GetComponentInChildren<SpawnArea>();
+            StartCoroutine(SpawnCoroutine());
+        }
+
+
+        private IEnumerator SpawnCoroutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(spawnDelayInSeconds);
+                Spawn(dummyObstaclePrefab);
+            }
+        }
+    }
+}
