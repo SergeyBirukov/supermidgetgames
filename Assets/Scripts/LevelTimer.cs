50eac021f21dfe326446257dbbc2c858aced95b8
SergeyBirukov <birukovsergej121@gmail.com>
2023-05-07 05:28:35 +0500

    Added switching between levels

---
+++ Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTimer: MonoBehaviour
+{
+    [SerializeField] private float levelTimeInSeconds = 30;
+    [SerializeField] private string nextLevelName;
+
+    private void Start()
+    {
+        StartCoroutine(LevelEndCountdown());
+    }
+
+    private IEnumerator LevelEndCountdown()
+    {
+        yield return new WaitForSeconds(levelTimeInSeconds);
+        EndLevel();
+    }
+
+    private void EndLevel()
+    {
+        SceneManager.LoadScene(nextLevelName);
+    }
+}
