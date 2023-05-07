3e3d281aa2423b0d1104c357fdad278c983a798c
lessy2014 <lessik03@gmail.com>
2023-05-07 07:31:09 +0500

    Gleb mode

---
+++ Assets/GlebMode.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlebMode : MonoBehaviour
+{
+    public bool IsGlebMode;
+    public static GlebMode Instance;
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this.gameObject);
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public void GlebModeSetActive()
+    {
+        IsGlebMode = !IsGlebMode;
+    }
+}
