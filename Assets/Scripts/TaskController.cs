c41e6e7f3413656009897f3696f8adf92346bbe5
SergeyBirukov <birukovsergej121@gmail.com>
2023-05-07 07:02:21 +0500

    Added first riddle to main game

--- Assets/Scripts/TaskController.cs
+++ Assets/Scripts/TaskController.cs
@@ -2,9 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TaskController : MonoBehaviour
 {
+    [SerializeField] private string nextLevelName;
+    [SerializeField] private Image screenCover;
+    private bool finished;
     private GameObject _uranium;
 
     private GameObject _goal;
@@ -23,6 +28,22 @@ public class TaskController : MonoBehaviour
         if (Math.Abs(_uranium.transform.position.x - _goal.transform.position.x) > 0.1f ||
             Math.Abs(_uranium.transform.position.y - _goal.transform.position.y) > 0.1f) return;
         var goalSprite = _goal.GetComponent<SpriteRenderer>();
+        if(finished) return;
+        finished = true;
         goalSprite.sprite = winCellSprite;
+        StartCoroutine(HideScreen());
+    }
+    
+    private IEnumerator HideScreen()
+    {
+        for (var i = 0; i < 5; i++)
+        {
+            yield return new WaitForSeconds(0.5f);
+            var tmp = screenCover.color;
+            tmp.a += 0.2f;
+            screenCover.color = tmp;
+        }
+
+        SceneManager.LoadScene(nextLevelName);
     }
 }
