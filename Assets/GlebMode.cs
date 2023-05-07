8481fbdd4258f50cba7bbd601d87a6536a092813
SergeyBirukov <birukovsergej121@gmail.com>
2023-05-07 08:10:57 +0500

    Added gleb to main menu

--- Assets/GlebMode.cs
+++ Assets/GlebMode.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GlebMode : MonoBehaviour
 {
     public bool IsGlebMode;
     public static GlebMode Instance;
+    
+    [SerializeField] private Sprite defaultDB;
+    [SerializeField] private Sprite glebBg;
+    [SerializeField] private Image BG;
+    
     void Awake()
     {
         if (Instance != null && Instance != this)
@@ -20,8 +26,21 @@ public class GlebMode : MonoBehaviour
         }
     }
 
+    private void UpdateBackground()
+    {
+        if (IsGlebMode)
+        {
+            BG.sprite = glebBg;
+        }
+        else
+        {
+            BG.sprite = defaultDB;
+        }
+    }
+
     public void GlebModeSetActive()
     {
         IsGlebMode = !IsGlebMode;
+        UpdateBackground();
     }
 }
